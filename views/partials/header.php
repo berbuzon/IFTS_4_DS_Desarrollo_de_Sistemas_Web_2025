8efe997b0d319a97884aefc3c9774b48f61127ae
Bernardo Eliezer <berbuzon@gmail.com>
2025-08-18 22:19:26 -0300

    cambio de carpeta

--- views/partials/header.php
+++ views/partials/header.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sistema de Usuarios</title>
     <!-- <link rel="stylesheet" href="../public/css/estilos.css" -->
-    <link rel="stylesheet" href="/proyecto_prueba/public/css/estilos.css">
+    <link rel="stylesheet" href="/IFTS_4_DS_Desarrollo_de_Sistemas_Web_2025/public/css/estilos.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
 </head>
 <body>
